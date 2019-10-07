2ac62d22b359c0759deb93e25fbaca297293614c
Tsyvinskyi Vasiliy <sodiicc@gmail.com>
2019-10-07 05:32:12 +0300

    del atlas

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://sodiicc:trader32@cluster0-rsnt4.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
