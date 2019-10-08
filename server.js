3fe814bf127cca85a2e718b99220b3331ca8bcbb
Tsyvinskyi Vasiliy <sodiicc@gmail.com>
2019-10-08 12:37:42 +0300

    change all

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require("path")
 
 require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(cors());
 app.use(express.json());
 
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static("backend/build"))
-}
+// if(process.env.NODE_ENV ==='production'){
+//   app.use(express.static("backend/build"))
+// }
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
@@ -27,6 +29,9 @@ const usersRouter = require('./routs/usersRouter');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on PORT: ${port}`)
